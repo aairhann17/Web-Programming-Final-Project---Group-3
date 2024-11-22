29ba5bfb3121392887e643e7c979ae66a9b68d72
aairhann17 <151668629+aairhann17@users.noreply.github.com>
2024-11-22 17:28:17 -0500

    Create user_input.js

---
+++ user_input.js
@@ -0,0 +1,15 @@
+//initialization of global variables
+const loginForm = document.getElementById("login-form");
+const loginBtn = document.getElementById("login-button");
+
+
+//event listener to log the user in
+loginBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    const username = loginForm.username.value;
+    const password = loginForm.password.value;
+
+    if(username === "" && password === "") {
+        location.reload();
+    }
+});
