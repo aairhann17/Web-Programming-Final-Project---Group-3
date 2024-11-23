f44394a43e7cfc3ec38ced3ba88701efcec88c40
Steven-Sunny <sunsteven796@gmail.com>
2024-11-23 14:55:33 -0500

    Started a new php file for user image uploading

---
+++ uploadimg.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial scale=1.0">
+	<title>Image Upload - A World Of Colour </title>
+	<link rel="stylesheet" href="styles.css">
+</head>
+<body class = "imageuploadbody">
+    
+</body>
