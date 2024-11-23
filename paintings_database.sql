20c556f0e73a87ce9974ea4dc2b36c4637ad93aa
Steven-Sunny <sunsteven796@gmail.com>
2024-11-23 17:30:33 -0500

    implemented parts of paintings_database.sql

--- paintings_database.sql
+++ paintings_database.sql
@@ -1,1 +1,18 @@
+drop database if exists worldofcolourdb;
 
+create database worldofcolourdb;
+use worldofcolourdb;
+
+create table userData (
+    ID int AUTO_INCREMENT primary key,
+    Username varchar(255) NOT NULL,
+    Email varchar(255) NOT NULL,
+    Password varchar(255) NOT NULL
+)
+
+create table painting(
+    ID int AUTO_INCREMENT primary key,
+    PaintingName varchar(255) NOT NULL,
+    Artist varchar(255) NOT NULL,
+    Year int NOT NULL
+)
