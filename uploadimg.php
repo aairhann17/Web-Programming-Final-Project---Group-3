0d43fa530de1e27a8ed6857033a3e30b89e3e413
Steven-Sunny <sunsteven796@gmail.com>
2024-11-23 17:52:41 -0500

    working on uploadimg.php

--- uploadimg.php
+++ uploadimg.php
@@ -6,6 +6,9 @@
 	<title>Image Upload - A World Of Colour </title>
 	<link rel="stylesheet" href="styles.css">
 </head>
-<body class = "imageuploadbody">
-    
+<body class = "image-upload-body">
+    <div class = "image-upload-body">
+		<h1>Please insert your image</h1>
+		<input type="file" accept="image/jpeg, image/png, image/jpg">
+	</div>
 </body>
